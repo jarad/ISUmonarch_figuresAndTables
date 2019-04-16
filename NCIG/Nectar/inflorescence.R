ebddd4c4c03f9d4dc9c4f7cf3a15436381268fd6
Jarad Niemi <niemi@iastate.edu>
2019-04-16 14:01:46 -0500

    Add inflorescence and richness plots

---
+++ NCIG/Nectar/inflorescence.R
@@ -0,0 +1,32 @@
+library("tidyverse")
+library("ISUmonarch")
+
+
+
+d <- nectar %>%
+  mutate(ymd = lubridate::ymd(paste(year,month,day,sep="-"))) %>%
+  group_by(ymd,siteID) %>%
+  summarize(inflorescence = sum(count),
+            richness = sum(count>0)) 
+
+# Inflorescence
+
+ggplot(d, aes(yearround, inflorescence)) + 
+  geom_point() +
+  facet_wrap(~siteID) +
+  theme_bw() + 
+  scale_y_log10() 
+
+ggplot(d, aes(ymd, inflorescence)) + 
+  geom_point() +
+  facet_wrap(~siteID) +
+  theme_bw() + 
+  scale_y_log10() 
+
+# Richness
+
+ggplot(d, aes(yearround, richness)) + 
+  geom_point() +
+  facet_wrap(~siteID) +
+  theme_bw() + 
+  scale_y_log10() 
