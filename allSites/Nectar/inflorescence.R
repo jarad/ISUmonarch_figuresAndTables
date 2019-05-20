1cfb869a759a324e9abb682fc698822b3a0661d5
Nehemias Ulloa <nulloa1@iastate.edu>
2019-05-20 16:01:33 -0500

    Avg across rounds

--- allSites/Nectar/inflorescence.R
+++ allSites/Nectar/inflorescence.R
@@ -5,21 +5,27 @@ library("ISUmonarch")
 
 d <- nectar %>%
   mutate(ymd = lubridate::ymd(paste(year,month,day,sep="-"))) %>%
-  group_by(ymd,siteID) %>%
+  group_by(year, transectID) %>%
   summarize(inflorescence = sum(count),
-            richness = sum(count>0)) 
+            richness = sum(count>0)) %>%
+  left_join(ISUmonarch::transect, by="transectID") %>%
+  filter(!(transectID %in% c("tuth1", "tuth2", "tsie1")))
+
+
+
 
 # Inflorescence
 
-ggplot(d, aes(yearround, inflorescence)) + 
+ggplot(d, aes(ymd, inflorescence)) + 
   geom_point() +
   facet_wrap(~siteID) +
   theme_bw() + 
   scale_y_log10() 
 
-ggplot(d, aes(ymd, inflorescence)) + 
-  geom_point() +
-  facet_wrap(~siteID) +
+ggplot(d, aes(year, inflorescence)) + 
+  geom_point(aes(color=transectID)) +
+  geom_line(aes(color=transectID)) +
+  facet_wrap(~grant) +
   theme_bw() + 
   scale_y_log10() 
 
