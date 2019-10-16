a31a3614e19ff3a322064c449a6c23f766c99905
Jarad Niemi <niemi@iastate.edu>
2019-10-16 14:10:52 -0700

    Add Robel to report

--- iacig/robel.R
+++ iacig/robel.R
@@ -1,5 +1,6 @@
 library("tidyverse")
 library("ISUmonarch")
+library("xtable")
 
 source("../ISUcolorPalette.R")
 
@@ -11,6 +12,9 @@ d <- robel %>%
          Month = factor(Month, levels = c("June","July","August")))
 
 stopifnot(all(d$censored == "not"))
+
+####################################################################
+# Plot by month
 
 s <- d %>%
   group_by(year, siteID, Month) %>%
@@ -27,7 +31,38 @@ g <- ggplot(s, aes(year, height,
   scale_color_manual(values = month_col)
 
 ggsave(plot = g,
-       filename = "robel.png")
+       filename = "robel_monthly.png")
+
+####################################################################
+# Plot by year
+
+s <- d %>%
+  group_by(year, siteID) %>%
+  summarize(height = mean(height))
+
+s %>% 
+  spread(year, height) %>%
+  xtable(
+    align = "ll|rrrr",
+    caption = "Mean Robel heights",
+    digits = 1) %>%
+  print(
+    file = "robel_yearly.tex",
+    include.rownames = FALSE)
+
+g <- ggplot(s, aes(year, height)) +
+  geom_line() + 
+  facet_wrap(~siteID) + 
+  labs(x = "Year", 
+       y = "Height (cm)",
+       title = "Mean Robel Heights") + 
+  theme(legend.position = "bottom") 
+
+ggsave(plot = g,
+       filename = "robel_yearly.png",
+       width = 6, height = 4)
+
+
 
 
   
