9fe73793b660d41bd5d9444bef4ab275a8212bc8
Jarad Niemi <niemi@iastate.edu>
2019-10-17 11:35:08 -0700

    Add nectar table

--- iacig/nectar.R
+++ iacig/nectar.R
@@ -1,5 +1,6 @@
 library("tidyverse")
 library("ISUmonarch")
+library("xtable")
 
 source("../ISUcolorPalette.R")
 
@@ -53,16 +54,16 @@ s <- d %>%
   group_by(year, siteID, month, common_name, native) %>%
   summarize(total = sum(count))
   
-ggplot(s, aes(year, total, 
-              color = common_name,
-              linetype = native,
-              group = common_name)) +
-  geom_line() + 
-  facet_grid(siteID ~ month) + 
-  labs(x = "Year", y = "Inflorescence", linetype = "Species") +
-  scale_y_log10() + 
-  theme_bw() + 
-  theme(legend.position = "bottom")
+# ggplot(s, aes(year, total, 
+#               color = common_name,
+#               linetype = native,
+#               group = common_name)) +
+#   geom_line() + 
+#   facet_grid(siteID ~ month) + 
+#   labs(x = "Year", y = "Inflorescence", linetype = "Species") +
+#   scale_y_log10() + 
+#   theme_bw() + 
+#   theme(legend.position = "bottom")
 
 ss <- s %>%
   group_by(year, siteID, common_name, native) %>%
@@ -73,3 +74,52 @@ ss <- s %>%
   arrange(native, log_ratio, common_name, siteID)
 
 ss %>% write_csv("nectar_ratio.csv")
+
+
+###################################################################
+# Create 
+
+ss <- s %>%
+  group_by(year, siteID, common_name, native) %>%
+  summarize(density = mean(total)) %>%
+  ungroup() %>%
+  mutate(native = factor(native, levels=c("Native","Introduced"))) %>%
+  
+  # cra1 has a 70m transect, so adjust to 100m density
+  mutate(density = ifelse(siteID == "cra1", density/.7, density)) %>%
+  unite("site_year", siteID, year, sep="@") %>%
+  spread(site_year, density, fill = NA) %>%
+  arrange(native, common_name) %>%
+  rename(`Common name` = "common_name")
+
+n_native = sum(ss$native == "Native")
+ss$native <- NULL
+
+# Fix column names and create additional row
+nc <- ncol(ss)
+tmp <- data.frame(nms = names(ss)[-1]) %>%
+  tidyr::separate(nms, into=c("row1", "row2"), sep="@")
+names(ss)[-1] <- tmp$row2
+new_row <- paste(" & \\multicolumn{4}{c|}{",unique(tmp$row1),"}", 
+                 collapse="")
+new_row <- gsub("_", " ", new_row)
+new_row <- paste(new_row, "\\\\", collapse=" ")
+
+# Write table to file
+ss %>%
+  xtable(
+    digits = 0,
+    align = paste("rl|",paste(rep("rrrr|",floor(nc/4)),collapse=""),sep="",collapse=""),
+    caption="Nectar: density (per 100m2) across all rounds. Species above the line are native and below the line are introduced.",
+    label = "t:nectar") %>%
+  print(
+    file = "nectar.tex",
+    include.rownames = FALSE, 
+    # NA.string="na",
+    add.to.row = list(pos = list(-1), 
+                      command = new_row),
+    caption.placement = "top",
+    size="\\small",
+    # floating.environment = "sidewaystable",
+    hline.after = c(-1,0,n_native,nrow(ss))
+  ) 
